d50814afa1f7b34491cde2dc6d13a4722cbe45c0
2016gary <grrbr@sina.com>
2017-10-11 22:00:58 +0800

    添加和修改

---
+++ FoF/reducer.py
@@ -0,0 +1,25 @@
+import sys
+
+current_key = None
+key = None
+current_count = []
+for line in sys.stdin:
+    line = line.strip()
+    key, count = line.split('\t', 1)
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+    if current_key == key:
+        current_count.append(count)
+    else:
+        if current_key:
+            current_count.sort()
+            if current_count[0] > 0:
+                print('%s\t%s' % (current_key, sum(current_count)))
+            current_count = [count]
+        else:
+            current_count.append(count)
+        current_key = key
+
+sys.exit(0)
